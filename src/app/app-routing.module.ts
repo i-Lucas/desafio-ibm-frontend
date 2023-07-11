1d3b3831d44d0b63175a62cca854a6dc6fe37a4a
i-Lucas <novo.contato.lucas@gmail.com>
2023-07-11 08:59:39 -0300

    feat: importing component modules to app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule) },
+  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
