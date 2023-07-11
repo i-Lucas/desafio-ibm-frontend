7a397f581d8af11de80daa474c1799720edbbc6d
i-Lucas <novo.contato.lucas@gmail.com>
2023-07-11 09:01:30 -0300

    fix: remove unnecessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
