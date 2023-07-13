77ab261f151c2d4b3b9b27937a4be89f7b830040
i-Lucas <novo.contato.lucas@gmail.com>
2023-07-13 05:51:40 -0300

    feat: candidate crud system

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { ApiErrorResponse, UserDetailsResponse } from 'src/app/interfaces/ApiResponse';
-import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ApiCandidateResponse } from 'src/app/interfaces/candidate';
+import { AuthenticationService } from 'src/app/services/auth/authentication.service';
+import { CandidateService } from 'src/app/services/candidates/candidate.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,71 +13,251 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 })
 export class DashboardComponent implements OnInit {
 
-  private username = "";
+  private username = '';
   protected isLoading = false;
-  authenticated: boolean = false;
-  constructor(private authService: AuthenticationService, private router: Router, private messageService: MessageService) { }
+  protected authenticated = false;
+  protected errors: { [key: string]: string } = {};
+  protected candidates: { id: string; name: string; email: string; status: string }[] = [];
+  protected newUser: { name: string; email: string; status: string } = { name: '', email: '', status: '' };
+
+  private token = '';
+  protected isLoadingButtonAddNewCandidate = false;
+  protected isLoadingButtonScheduleCandidate = false;
+  protected isLoadingButtonApproveCandidate = false;
+  protected isLoadingButtonDisqualifyCandidate = false;
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router,
+    private messageService: MessageService,
+    private candidateService: CandidateService,
+  ) { }
 
   ngOnInit(): void {
     this.performAuthentication();
   }
 
-  private navigateToLogin() {
-    this.router.navigate(['/']);
-  }
+  protected addUser() {
 
-  showSuccess(username: string) {
-    this.messageService.add({
-      severity: 'success',
-      summary: 'Success',
-      detail: 'Bem Vindo '.concat(username)
+    if (this.buttonAddNewCandidateIsDisabled()) return;
+    const candidate = { name: this.newUser.name, email: this.newUser.email };
+    this.isLoadingButtonAddNewCandidate = true;
+
+    this.candidateService.addCandidate(candidate, this.token).subscribe({
+
+      next: (response: ApiCandidateResponse) => {
+        this.handleAddCandidateSuccess(response);
+      },
+      error: (error: ApiErrorResponse) => {
+        this.handleAddCandidateError(error);
+      },
+      complete: () => {
+        this.isLoadingButtonAddNewCandidate = false;
+      }
     });
   }
 
-  showError(detail: string) {
-    const message = detail === undefined ?
-      "Algo de errado aconteceu ! Tente novamente mais tarde" : detail;
+  protected schedule(candidateId: string, status: string) {
 
-    this.messageService.add({
-      severity: 'error',
-      summary: 'Error',
-      detail: message
+    if (this.isScheduleButtonDisabled(status)) return;
+    this.isLoadingButtonScheduleCandidate = true;
+
+    this.candidateService.scheduleCandidate(candidateId, this.token).subscribe({
+
+      next: (response: ApiCandidateResponse) => {
+        this.handleScheduleCandidateSuccess(response, candidateId);
+      },
+      error: (error: ApiErrorResponse) => {
+        this.handleScheduleCandidateError(error);
+      },
+      complete: () => {
+        this.isLoadingButtonScheduleCandidate = false;
+      }
     });
   }
 
-  performAuthentication(): void {
+  protected approve(candidateId: string, status: string) {
 
-    const token = this.authService.getToken();
-    if (token) {
+    if (this.isApproveButtonDisabled(status)) return;
+    this.isLoadingButtonApproveCandidate = true;
 
-      this.authService.getUserDetails(token)
-        .subscribe({
-          next: this.handleSuccessResponse,
-          error: this.handleErrorResponse,
-        })
+    this.candidateService.approveCandidate(candidateId, this.token).subscribe({
 
+      next: (response: ApiCandidateResponse) => {
+        this.handleApproveCandidateSuccess(response, candidateId);
+      },
+      error: (error: ApiErrorResponse) => {
+        this.handleApproveCandidateError(error);
+      },
+      complete: () => {
+        this.isLoadingButtonApproveCandidate = false;
+      }
+    });
+  }
+
+  protected disqualify(candidateId: string, status: string) {
+
+    if (this.isDisqualifyButtonDisabled(status)) return;
+    this.isLoadingButtonDisqualifyCandidate = true;
+
+    this.candidateService.disqualifyCandidate(candidateId, this.token).subscribe({
+
+      next: (response: ApiCandidateResponse) => {
+        this.handleDisqualifyCandidateSuccess(response, candidateId);
+      },
+      error: (error: ApiErrorResponse) => {
+        this.handleDisqualifyCandidateError(error);
+      },
+      complete: () => {
+        this.isLoadingButtonDisqualifyCandidate = false;
+      }
+    });
+  }
+
+  private showSuccess(detail: string) {
+    setTimeout(() => {
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail,
+      });
+    }, 100);
+  }
+
+  private showError(detail: string) {
+    const message = detail === undefined ? 'Algo de errado aconteceu! Tente novamente mais tarde' : detail;
+    setTimeout(() => {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: message
+      });
+    }, 100);
+  }
+
+  private performAuthentication(): void {
+    const userToken = this.authService.getToken();
+    if (userToken) {
+      this.token = userToken;
+      this.authService.getUserDetails(userToken).subscribe({
+        next: (response: UserDetailsResponse) => {
+          this.handleSuccessResponse(response);
+        },
+        error: (error: ApiErrorResponse) => {
+          this.handleErrorResponse(error);
+        }
+      });
     } else {
-      console.error("UNAUTHORIZED");
+      console.warn('UNAUTHORIZED');
       this.handleExpiredSession();
     }
   }
 
-  handleExpiredSession(): void {
-    setTimeout(() => this.showError("Sessão expirada"), 0);
+  private handleExpiredSession(): void {
+    setTimeout(() => this.showError('Sessão expirada'), 0);
     setTimeout(() => this.navigateToLogin(), 1000);
   }
 
   private handleSuccessResponse = (response: UserDetailsResponse): void => {
+    this.candidates = response.candidates;
     this.isLoading = false;
     this.authenticated = true;
     this.username = response.email;
-    setTimeout(() => this.showSuccess(this.username), 10);
+    const message = 'Bem Vindo '.concat(this.username);
+    this.showSuccess(message);
   };
 
-  private handleErrorResponse = (error: ApiErrorResponse): void => {
+  private handleErrorResponse = ({ error }: ApiErrorResponse): void => {
     this.isLoading = false;
-    console.error(error.error.message);
-    this.showError(error.error.message);
+    this.showError(error.message);
+    console.warn(error);
+    if (error.status === 'NOT_FOUND') {
+      this.navigateToLogin();
+    }
   };
+
+  private handleAddCandidateSuccess(response: ApiCandidateResponse): void {
+    this.showSuccess(response.message);
+    this.isLoadingButtonAddNewCandidate = false;
+    this.candidates.push({
+      name: this.newUser.name,
+      email: this.newUser.email,
+      status: 'Received',
+      id: response.candidateId
+    });
+    this.clearForm();
+  }
+
+  private handleAddCandidateError({ error }: ApiErrorResponse): void {
+    this.showError(error.message);
+    this.isLoadingButtonAddNewCandidate = false;
+  }
+
+  private handleScheduleCandidateSuccess(response: ApiCandidateResponse, candidateId: string): void {
+    const candidate = this.candidates.find(c => c.id === candidateId);
+    this.showSuccess(response.message);
+    this.isLoadingButtonScheduleCandidate = false;
+    if (candidate) { candidate.status = 'Qualified'; };
+  }
+
+  private handleScheduleCandidateError({ error }: ApiErrorResponse): void {
+    this.showError(error.message);
+    this.isLoadingButtonScheduleCandidate = false;
+  }
+
+  private handleApproveCandidateSuccess(response: ApiCandidateResponse, candidateId: string): void {
+    const candidate = this.candidates.find(c => c.id === candidateId);
+    this.showSuccess(response.message);
+    this.isLoadingButtonApproveCandidate = false;
+    if (candidate) { candidate.status = 'Approved'; };
+  }
+
+  private handleApproveCandidateError({ error }: ApiErrorResponse): void {
+    this.showError(error.message);
+    this.isLoadingButtonApproveCandidate = false;
+  }
+
+  private handleDisqualifyCandidateSuccess(response: ApiCandidateResponse, candidateId: string): void {
+    const candidate = this.candidates.find(c => c.id === candidateId);
+    this.showSuccess(response.message);
+    this.isLoadingButtonDisqualifyCandidate = false;
+    if (candidate) { candidate.status = 'Disqualified'; };
+  }
+
+  private handleDisqualifyCandidateError({ error }: ApiErrorResponse): void {
+    this.showError(error.message);
+    this.isLoadingButtonDisqualifyCandidate = false;
+  }
+
+  protected isScheduleButtonDisabled(status: string): boolean {
+    return status != 'Received';
+  }
+
+  protected isApproveButtonDisabled(status: string): boolean {
+    return status === 'Received' || status === 'Approved';
+  }
+
+  protected isDisqualifyButtonDisabled(status: string): boolean {
+    return status === 'Received' || status == 'Disqualified'
+  }
+
+  protected buttonAddNewCandidateIsDisabled(): boolean {
+    return !this.validateEmailFormat(this.newUser.email) || this.newUser.name.trim() === '';
+  }
+
+  private clearForm() {
+    this.newUser.name = '';
+    this.newUser.email = '';
+    this.errors = {};
+  }
+
+  private validateEmailFormat(email: string) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
+
+  private navigateToLogin() {
+    this.router.navigate(['/']);
+  }
 
 }
